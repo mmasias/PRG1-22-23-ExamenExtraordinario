1efd93e94db7d22424fc9e8eb14500768f17b93b
inaki <inaki.tazon@alumnos.uneatlantico.es>
2023-07-03 08:59:02 +0200

    empezamos con el movimiento

--- TazónInaki/src/Main.java
+++ TazónInaki/src/Main.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 
-	public class Main {
-	    public static final int NUM_ROWS = 15;
-	    public static final int NUM_COLS = 10;
-	    private double[][] cells = new double[NUM_ROWS][NUM_COLS];
+public class Main {
+    public static final int NUM_ROWS = 15;
+    public static final int NUM_COLS = 10;
+    private double[][] cells = new double[NUM_ROWS][NUM_COLS];
 
-	    private int currentRow = 0;
-	    private int currentCol = 0;
+    private int currentRow = 0;
+    private int currentCol = 0;
 
-	    public void run() {
-	        Scanner scanner = new Scanner(System.in);
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
 
-	
-	
-	
-	
-	
-	
+        while (true) {
+            printCells();
+            System.out.print("Introduce un comando (w/a/s/d para mover, f para salir, Enter para ingresar valor): ");
+            String command = scanner.next();
+            switch (command.toLowerCase()) {
+                case "w":
+                    if (currentRow > 0) {
+                        currentRow--;
+                    }
+                    break;
+                case "a":
+                    if (currentCol > 0) {
+                        currentCol--;
+                    }
+                    break;
+  
