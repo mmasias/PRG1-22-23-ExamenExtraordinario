d284a719de0502f204ac3d16c13de9b79348d7b2
anibalbg <anibal.bayas@alumnos.uneatlantico.es>
2023-07-03 10:12:16 +0200

    Intentando editar las casillas

--- bayasAnibal/Main.java
+++ bayasAnibal/Main.java
@@ -7,8 +7,8 @@ public class Main {
         char inputUsuario;
         boolean terminar = false;
 
-        char[][] unaMatriz = {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        int[][] unaMatriz = {
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0,},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
@@ -26,7 +26,7 @@ public class Main {
 
         };
 
-        int[] Posicion = {0, 0};
+        int[] posicion = {0, 0};
 
         int limiteInferior = 0;
         int limiteSuperior = unaMatriz.length - 1;
@@ -37,76 +37,75 @@ public class Main {
         do {
             for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
                 for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
-                    if (laFila == Posicion[0] && laColumna == Posicion[1]) {
-                        imprimePacman(skin);
+                    if (laFila == posicion[0] && laColumna == posicion[1]) {
+                        posicionActual(skin);
                     } else {
-                        imprimeTerreno(unaMatriz[laFila][laColumna], skin);
+                        hojaCalculo(unaMatriz[laFila][laColumna], skin);
                     }
                 }
                 System.out.println();
             }
+            System.out.println("Presione WASD para moverse, E para editar y F para terminar");
 
             inputUsuario = entrada.nextLine().charAt(0);
             switch (inputUsuario) {
                 case 's', 'S', '8':
-                    Posicion[0] = Posicion[0] + 1;
+                    posicion[0] = posicion[0] + 1;
                     break;
                 case 'w', 'W', '2':
-                    Posicion[0] = Posicion[0] - 1;
+                    posicion[0] = posicion[0] - 1;
                     break;
                 case 'a', 'A', '4':
-                    Posicion[1] = Posicion[1] - 1;
+                    posicion[1] = posicion[1] - 1;
                     break;
                 case 'd', 'D', '6':
-                    Posicion[1] = Posicion[1] + 1;
+                    posicion[1] = posicion[1] + 1;
                     break;
-                case 'v', 'V':
-                    skin = skin + 1;
-                    if (skin > 2) {
-                        skin = 0;
-                    }
+                case 'E', 'e':
+                    System.out.println("Ingrese el nuevo valor de la casilla");
+
+
                     break;
                 case 'f', 'F':
+                    System.out.println("Fin de la hoja de calculo");
                     terminar = true;
             }
 
-            if (Posicion[0] < limiteInferior) {
-                Posicion[0] = limiteSuperior;
+            if (posicion[0] < limiteInferior) {
+                posicion[0] = limiteSuperior;
             }
-            if (Posicion[0] > limiteSuperior) {
-                Posicion[0] = limiteInferior;
+            if (posicion[0] > limiteSuperior) {
+                posicion[0] = limiteInferior;
             }
-            if (Posicion[1] < limiteIzquierdo) {
-                Posicion[1] = limiteDerecho;
+            if (posicion[1] < limiteIzquierdo) {
+                posicion[1] = limiteDerecho;
             }
-            if (Posicion[1] > limiteDerecho) {
-                Posicion[1] = limiteIzquierdo;
+            if (posicion[1] > limiteDerecho) {
+                posicion[1] = limiteIzquierdo;
             }
 
-            if (unaMatriz[Posicion[0]][Posicion[1]] == 2) {
-                unaMatriz[Posicion[0]][Posicion[1]] = 0;
-            } else if (unaMatriz[Posicion[0]][Posicion[1]] == 4) {
-                unaMatriz[Posicion[0]][Posicion[1]] = 0;
+            if (unaMatriz[posicion[0]][posicion[1]] == 2) {
+                unaMatriz[posicion[0]][posicion[1]] = 0;
+            } else if (unaMatriz[posicion[0]][posicion[1]] == 4) {
+                unaMatriz[posicion[0]][posicion[1]] = 0;
             }
 
 
         } while (!terminar);
     }
 
-    static void imprimeTerreno(int unTile, int skin) {
+    static void hojaCalculo(int unTile, int skin) {
 
         String[][] terreno = {
-                {" . ",},
-
+                {" . ",},{"2222"}
         };
         System.out.print(terreno[unTile][skin]);
     }
 
-    static void imprimePacman(int skin) {
-        String[] pacman = {" - "};
-        System.out.print(pacman[skin]);
+    static void posicionActual(int skin) {
+        String[] posActual= {" - "};
+        System.out.print(posActual[skin]);
     }
-
 
 
 }
