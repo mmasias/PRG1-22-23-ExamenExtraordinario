6d281ecee854a5dd477fc8f29cacd1106fdb4ac5
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2023-07-03 09:16:26 +0200

    Update HojaDeCalculo.java

--- mauraMartinez/HojaDeCalculo.java
+++ mauraMartinez/HojaDeCalculo.java
@@ -9,9 +9,16 @@ public class HojaCalculo{
         int[][] Hoja = new int [15][10];
         char inputUsuario;
         boolan salirDelSistema = false; 
+        int filaActual = 0;
+        int columnaActual = 0;
+        
 
 
         do{
+            System.out.println("Posición actual: (" + filaActual + ", " + columnaActual + ")");
+            System.out.println("Valor actual: " + hojaCalculo[filaActual][columnaActual]);
+            System.out.print("Ingrese un comando (w/a/s/d) o presione Enter para entrar o F para salir del sistema: ");
+            String comando = scanner.nextLine();
             
         }while(!salirDelSistema);
         
