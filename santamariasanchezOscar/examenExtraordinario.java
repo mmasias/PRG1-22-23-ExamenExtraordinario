f39905b434a377aa2a169547bdca2c73b194736f
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2023-07-03 09:07:37 +0200

    commit 9:07 examenExtraordinario.java

--- santamariasanchezOscar/examenExtraordinario.java
+++ santamariasanchezOscar/examenExtraordinario.java
@@ -4,29 +4,33 @@ public class Main {
     public static void main(String[] args) {
         int[][] hojaCalculo= new int[15][10];
         Scanner s= new Scanner(System.in);
+       
+        System.out.println(imprimirTabla());
+
+
 
         for (int i=0; i<hojaCalculo.length; i++){
             for (int j=0; j<hojaCalculo[i].length; j++){
                 hojaCalculo[i][j]=0;
 
                 switch (s.next()){
-                    case "w"{
+                    case "w":{
                         j=j+1;
                         break;
                     }
-                    case "a"{
+                    case "a":{
                     i=i-1;
                     break;
                     }
-                    case"s"{
+                    case"s":{
                     j=j-1;
                     break;
                     }
-                    case "d"{
+                    case "d":{
                     i=i+1;
                     break;
                     }
-                    case "f"{
+                    case "f":{
                     hojaCalculo[i][j]=s.nextInt();
                     }
                 }
@@ -34,9 +38,33 @@ public class Main {
                 if (i >15 || j>10){
                     System.out.println("Error");
                 }
-                
+
+
+
             }
         }
 
     }
+    public static int imprimirTabla(){
+       String[][] tabla= {
+        System.out.println("+----------------------------------------------------+");
+        System.out.println("|  |  A |  B |  C |  D |  E |  F |  G |  H |  I |  J |");
+        System.out.println("+----------------------------------------------------+");
+        System.out.println("|  1|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  2|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  3|    |    |    |    |    |    |    |    |    |    |");    
+        System.out.println("|  4|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  5|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  6|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  7|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  8|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("|  9|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("| 10|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("| 11|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("| 12|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("| 14|    |    |    |    |    |    |    |    |    |    |");
+        System.out.println("| 13|    |    |    |    |    |    |    |    |    |    |");
+    }
+    }
+
 }
