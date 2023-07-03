5eebcb691f92ac25a720f83465094fb1d5fd27f7
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2023-07-03 09:00:41 +0200

    Create HojaDeCalculo.java

---
+++ mauraMartinez/HojaDeCalculo.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class HojaCalculo{
+
+    public static void main(String[] args){
+
+        Sacanner entrada = new Scanner(System.in);
+
+        int[][] Hoja = new int [15][10];
+        char inputUsuario;
+        boolan salirDelSistema = false; 
+
+
+        do{
+            
+        }while(!salirDelSistema);
+        
+
+
+        inputUsuario = entrada.nextLine().charAt(0);
+            switch (inputUsuario) {
+                case 's', 'S', '8':
+                    
+                    break;
+                case 'w', 'W', '2':
+                  
+                    break;
+                case 'a', 'A', '4':
+                    
+                    break;
+                case 'd', 'D', '6':
+                    
+                    break;
+
+                    case 'v', 'V':
+                        skin= skin + 1;
+  
+               
+                    case 'f', 'F':
+                    salirDelSistema = true;
+            }
+
+
+
+
+        
+         
+
+        
+    }
+}
