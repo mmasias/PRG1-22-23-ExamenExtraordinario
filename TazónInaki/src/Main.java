031a31ddb7b2947bd685f11d91acbcc9c13c7dbb
inaki <inaki.tazon@alumnos.uneatlantico.es>
2023-07-03 08:53:40 +0200

    vamos empezando

---
+++ TazónInaki/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+	public class Main {
+	    public static final int NUM_ROWS = 15;
+	    public static final int NUM_COLS = 10;
+	    private double[][] cells = new double[NUM_ROWS][NUM_COLS];
+
+	    private int currentRow = 0;
+	    private int currentCol = 0;
+
+	    public void run() {
+	        Scanner scanner = new Scanner(System.in);
+
+	
+	
+	
+	
+	
+	
