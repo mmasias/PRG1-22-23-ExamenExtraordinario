9811c5d5c2af15fa4b3b3e5ead3c65e8313e248d
Ana Deb <ana.diaz@alumnos.uneatlantico.es>
2023-07-03 09:51:14 +0200

    3ºcommit

--- DíazdeEntresotosBalbontínAna/VisiCalc.java
+++ DíazdeEntresotosBalbontínAna/VisiCalc.java
@@ -4,12 +4,12 @@ public class VisiCalc {
     public static void main(String[] args) {
         int filas = 15;
         int columnas = 10;
-        char[][] matriz = new char[filas][columnas];
 
         Scanner scanner = new Scanner(System.in);
         boolean ejecucion = true;
         int filaActual = 0;
         int columnaActual = 0;
+      
 
         //tengo que dibujar la matriz
         int[][] VisiCalc = {
@@ -32,23 +32,23 @@ public class VisiCalc {
 		};
         // IMPRIMO EL MAPA
 						// INICIO DEL PARSEO
-						if      (unArray2D[miFila][miColumna]==0)	{System.out.print(" + ");}	// Esquinas
-						else if (unArray2D[miFila][miColumna]==1)	{System.out.print("-");}	    // Paredes
-						else if (unArray2D[miFila][miColumna]==2)	{System.out.print("|");}	    // Espacios
-						else if (unArray2D[miFila][miColumna]==3)	{System.out.print("1");}	    // A
-						else if (unArray2D[miFila][miColumna]==4)	{System.out.print("2");}	    // B
-						else if (unArray2D[miFila][miColumna]==5)	{System.out.print("3");}	    // C
-						else if (unArray2D[miFila][miColumna]==6)	{System.out.print("4");}	    // D
-                        else if (unArray2D[miFila][miColumna]==7)	{System.out.print("1");}	    // E
-						else if (unArray2D[miFila][miColumna]==8)	{System.out.print("2");}	    // F
-						else if (unArray2D[miFila][miColumna]==9)	{System.out.print("3");}	    // G
-						else if (unArray2D[miFila][miColumna]==10)	{System.out.print("4");}	    // H
-                        else if (unArray2D[miFila][miColumna]==11)	{System.out.print("5");}	    // I
-						else if (unArray2D[miFila][miColumna]==12)	{System.out.print("6");}	    // J
-                        else if (unArray2D[miFila][miColumna]==13)	{System.out.print("7");}	    // E
-						else if (unArray2D[miFila][miColumna]==14)	{System.out.print("8");}	    // F
-						else if (unArray2D[miFila][miColumna]==15)	{System.out.print("9");}	    // G
-						else if (unArray2D[miFila][miColumna]==16)	{System.out.print("10");}	    // H
+						if      (VisiCalc[filas][columnas]==0)	{System.out.print(" + ");}	// Esquinas
+						else if (VisiCalc[filas][columnas]==1)	{System.out.print("-");}	    // Paredes
+						else if (VisiCalc[filas][columnas]==2)	{System.out.print("|");}	    // Espacios
+						else if (VisiCalc[filas][columnas]==3)	{System.out.print("1");}	    // A
+						else if (VisiCalc[filas][columnas]==4)	{System.out.print("2");}	    // B
+						else if (VisiCalc[filas][columnas]==5)	{System.out.print("3");}	    // C
+						else if (VisiCalc[filas][columnas]==6)	{System.out.print("4");}	    // D
+                        else if (VisiCalc[filas][columnas]==7)	{System.out.print("1");}	    // E
+						else if (VisiCalc[filas][columnas]==8)	{System.out.print("2");}	    // F
+						else if (VisiCalc[filas][columnas]==9)	{System.out.print("3");}	    // G
+						else if (VisiCalc[filas][columnas]==10)	{System.out.print("4");}	    // H
+                        else if (VisiCalc[filas][columnas]==11)	{System.out.print("5");}	    // I
+						else if (VisiCalc[filas][columnas]==12)	{System.out.print("6");}	    // J
+                        else if (VisiCalc[filas][columnas]==13)	{System.out.print("7");}	    // E
+						else if (VisiCalc[filas][columnas]==14)	{System.out.print("8");}	    // F
+						else if (VisiCalc[filas][columnas]==15)	{System.out.print("9");}	    // G
+						else if (VisiCalc[filas][columnas]==16)	{System.out.print("10");}	    // H
 
 						// FIN DEL PARSEO
 					
@@ -64,7 +64,35 @@ public class VisiCalc {
             // Obtener el comando del usuario
             System.out.println("CMDs: [W]Arriba,[S]Abajo,[A]Izquierda,[D]Derecha,[F]Salir");
             System.out.println("Inserte comando: ");
+            System.out.println("X: ["+filas+"] / Y: ["+columnas+"]");
             String comando = scanner.nextLine();
+          
         }
+
+        // Procesar el comando
+       
+            if (comando.equalsIgnoreCase("W")) {
+                if (filaActual > 0) {
+                    filaActual--;
+                }
+            } else if (comando.equalsIgnoreCase("A")) {
+                if (columnaActual > 0) {
+                    columnaActual--;
+                }
+            } else if (comando.equalsIgnoreCase("S")) {
+                if (filaActual < filas - 1) {
+                    filaActual++;
+                }
+            } else if (comando.equalsIgnoreCase("D")) {
+                if (columnaActual < columnas - 1) {
+                    columnaActual++;
+                }
+            } else if (comando.equalsIgnoreCase("Entrar")) {
+                System.out.print("Ingrese un texto: ");
+                String texto = scanner.nextLine();
+            }
+
+        //
+
     }
 }
