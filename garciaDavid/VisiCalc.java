5529354420767dfaab78d3385ff977df9ec69634
DavidGarciaCosta <114480378+DavidGarciaCosta@users.noreply.github.com>
2023-07-03 09:54:20 +0200

    Add files via upload
    algún pequeño cambio sin mucha importancia

--- garciaDavid/VisiCalc.java
+++ garciaDavid/VisiCalc.java
@@ -1,84 +1,85 @@
 import java.util.Scanner;
 
-public class VisiCalc {
+public class VisiCalc{
     public static void main(String[] args) {
-        int numeroColumna = 10;
-        int numeroFila = 15;
+        int numeroColumnas = 10;
+        int numeroFilas = 15;
         int columnaActual = 0;
-        int filaActual = 0;
-        int[][] excel = new int[numeroFila][numeroColumna];
-        Scanner scanner= new Scanner(System.in);
-        boolean corre= true;
-        while (corre){
+        int filaActual= 0;
+        String[][] excel = new String[numeroFilas][numeroColumnas];
+        Scanner scanner = new Scanner(System.in);
+
+        boolean corre = true;
+        while (corre) {
             imprimirExcel(excel, filaActual, columnaActual);
 
-            System.out.println("Ingrese un comando (w, a, s, d, f): ");
-            String comando= scanner.nextLine();
-            switch (comando){
+            System.out.print("Ingrese un comando (w, a, s, d, f): ");
+            String command = scanner.nextLine();
+
+            switch (command) {
                 case "w":
-                    if (numeroFila>0){
-                        numeroFila--;
+                    if (filaActual > 0) {
+                        filaActual--;
                     }
                     break;
                 case "a":
-                    if (numeroColumna>0){
-                        numeroColumna--;
+                    if (columnaActual > 0) {
+                        columnaActual--;
                     }
                     break;
                 case "s":
-                    if (numeroFila<excel.length-1){
-                        numeroFila++;
+                    if (filaActual < numeroFilas - 1) {
+                        filaActual++;
                     }
                     break;
                 case "d":
-                    if (numeroColumna<excel.length-1){
-                        numeroColumna++;
+                    if (columnaActual < numeroColumnas - 1) {
+                        columnaActual++;
                     }
                     break;
                 case "f":
-                    corre=false;
+                    corre = false;
                     break;
                 default:
-                    if (comando.length()>6){
-                        excel[filaActual][columnaActual]= Integer.parseInt(comando.substring(0,6));
-                    }else{
-                        String valorCelda= String.format("%6s", comando);
-                        excel[filaActual][columnaActual]= Integer.parseInt(valorCelda);
-                    } break;
+                    if (command.length() > 6) {
+                        excel[filaActual][columnaActual] = command.substring(0, 6);
+                    } else {
+                        String cellValue = String.format("%6s", command);
+                        excel[filaActual][columnaActual] = cellValue;
+                    }
+                    break;
             }
-            System.out.println("Programa finalizado.");
-            scanner.close();
+        }
 
+        System.out.println("Programa finalizado.");
+        scanner.close();
     }
 
+    public static void imprimirExcel(String[][] excel, int filaActual, int columnaActual) {
+        int numeroFilas = excel.length;
+        int numeroColumnas = excel[0].length;
 
-        public static void imprimirExcel(int[][] excel, int filaActual, int columnaActual){
-            int numeroFila= excel.length;
-            int numeroColumna= excel[0].length;
-            System.out.println("Hoja de cálculo:");
-            for(int Fila=0; Fila<numeroFila; Fila++){
-            for(int Columna=0; Columna<numeroColumna; Columna++){
-                if (Fila==filaActual && Columna==columnaActual){
+        System.out.println("Hoja de cálculo:");
+
+        for (int row = 0; row < numeroFilas; row++) {
+            for (int col = 0; col < numeroColumnas; col++) {
+                if (row == filaActual && col == columnaActual) {
                     System.out.print("X");
-                }else{
+                } else {
                     System.out.print("/l");
                 }
-                System.out.print(excel[Fila][Columna]);
+
+                System.out.print(excel[row][col]);
                 System.out.print("/l");
             }
-            System.out.println();
 
+            System.out.println();
         }
-            System.out.println();
-            System.out.println("X: " + (filaActual + 1));
-            System.out.println("Y: " + (columnaActual + 1));
-            System.out.println();
+
+        System.out.println();
+
+        System.out.println("X: " + (filaActual + 1));
+        System.out.println("Y: " + (columnaActual + 1));
+        System.out.println();
     }
-
-
 }
-
-
-}
-
-
