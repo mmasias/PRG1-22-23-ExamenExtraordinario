e777f89b9372e0374152427f0653adcd27b4011d
Aurelio Ochoa <61554216+aurelioochoa@users.noreply.github.com>
2023-07-03 10:28:59 +0200

    Update visiCalc.java

--- visiCalc.java
+++ visiCalc.java
@@ -4,27 +4,27 @@ public class VisiCalc {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
-        char inputUsuario;
-        boolean finAplicacion = false;
-
-        int[][] hojaDeCalculo = new int[15][10];
+        String[][] hojaDeCalculo = new String[15][10];
 
         int[] posicionCursor = { 0, 0 };
 
-        int limiteInferior=0;
-        int limiteSuperior=hojaDeCalculo.length-1;
-        int limiteIzquierdo=0;
-        int limiteDerecho=hojaDeCalculo[0].length-1;
+        int limiteInferior = 0;
+        int limiteSuperior = hojaDeCalculo.length - 1;
+        int limiteIzquierdo = 0;
+        int limiteDerecho = hojaDeCalculo[0].length - 1;
+
+        boolean finAplicacion = false;
 
         do {
             imprimirEncabezado();
             for (int fila = 0; fila < hojaDeCalculo.length; fila++) {
                 imprimirNumeroFila(fila);
                 for (int columna = 0; columna < hojaDeCalculo[fila].length; columna++) {
+                    String dato = hojaDeCalculo[fila][columna];
                     if (fila == posicionCursor[0] && columna == posicionCursor[1]) {
-                        System.out.print("[____]");
+                        System.out.print("[" + formatoCelda(dato) + "]");
                     } else {
-                        System.out.print("      ");
+                        System.out.print(" " + formatoCelda(dato) + " ");
                     }
                 }
                 System.out.print("|");
@@ -32,55 +32,69 @@ public class VisiCalc {
             }
             System.out.println("+" + "-".repeat(62) + "+");
 
-            inputUsuario = Character.toLowerCase(entrada.nextLine().charAt(0));
-            switch (inputUsuario) {
+            String inputUsuario = entrada.nextLine();
+            char comando = inputUsuario.length() > 0 ? inputUsuario.charAt(0) : ' ';
+            
+            if (comando == 'i') {
+                System.out.print("Introduce el dato: ");
+                String dato = entrada.nextLine();
+                hojaDeCalculo[posicionCursor[0]][posicionCursor[1]] = dato;
+            }
+            
+            switch (comando) {
                 case 's':
-                    posicionCursor[0] = posicionCursor[0] + 1;
+                    posicionCursor[0] = Math.min(posicionCursor[0] + 1, limiteSuperior);
                     break;
                 case 'w':
-                    posicionCursor[0] = posicionCursor[0] - 1;
+                    posicionCursor[0] = Math.max(posicionCursor[0] - 1, limiteInferior);
                     break;
                 case 'a':
-                    posicionCursor[1] = posicionCursor[1] - 1;
+                    posicionCursor[1] = Math.max(posicionCursor[1] - 1, limiteIzquierdo);
                     break;
                 case 'd':
-                    posicionCursor[1] = posicionCursor[1] + 1;
+                    posicionCursor[1] = Math.min(posicionCursor[1] + 1, limiteDerecho);
                     break;
                 case 'f':
                     finAplicacion = true;
+                    break;
             }
-
-            if (posicionCursor[0]<limiteInferior) {posicionCursor[0]=limiteSuperior;}
-            if (posicionCursor[0]>limiteSuperior) {posicionCursor[0]=limiteInferior;}
-            if (posicionCursor[1]<limiteIzquierdo) {posicionCursor[1]=limiteDerecho;}
-            if (posicionCursor[1]>limiteDerecho) {posicionCursor[1]=limiteIzquierdo;}
 
         } while (!finAplicacion);
     }
 
-    static void imprimirGrafico(int assetID){
+    static void imprimirGrafico(int assetID) {
         String[][] graficos = {
-            {"[____]", "+", "-", "|"}
+                {"[____]", "+", "-", "|"}
         };
         System.out.print(graficos[assetID]);
     }
 
     static void imprimirEncabezado() {
-      System.out.println("+" + "-".repeat(62) + "+");
-      System.out.print("|  |");
-      for (char letra = 'A'; letra < 'A' + 10; letra++) {
-        System.out.print("    " + letra + "|");
-      }
-      System.out.println("\n+" + "-".repeat(62) + "+");
+        System.out.println("+" + "-".repeat(62) + "+");
+        System.out.print("|  |");
+        for (char letra = 'A'; letra < 'A' + 10; letra++) {
+            System.out.print("    " + letra + "|");
+        }
+        System.out.println("\n+" + "-".repeat(62) + "+");
     }
 
     static void imprimirNumeroFila(int fila) {
         int filaHoja = fila + 1;
         String pared = (String.valueOf(filaHoja).length() < 2) ? "| " : "|";
-        if (fila == 10){
-            System.out.print( "|" + (filaHoja) + "|");
+        if (fila == 10) {
+            System.out.print("|" + (filaHoja) + "|");
         } else {
-            System.out.print( pared + (filaHoja) + "|");
+            System.out.print(pared + (filaHoja) + "|");
+        }
+    }
+
+    static String formatoCelda(String dato) {
+        if (dato == null) {
+            return "      ";
+        } else if (dato.length() < 6) {
+            return " ".repeat(6 - dato.length()) + dato;
+        } else {
+            return dato.substring(0, 6);
         }
     }
 }
