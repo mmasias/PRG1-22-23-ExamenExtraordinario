85ef3a1f210a51a78b41640c79f29f1d081cedba
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2023-07-03 09:53:11 +0200

    Update HojaDeCalculo.java

--- mauraMartinez/HojaDeCalculo.java
+++ mauraMartinez/HojaDeCalculo.java
@@ -11,16 +11,21 @@ public class HojaCalculo{
         boolan salirDelSistema = false; 
         int filaActual = 0;
         int columnaActual = 0;
+        int filas=15;
+        int columnas = 10; 
         
 
 
         do{
+            
             System.out.println("Posición actual: (" + filaActual + ", " + columnaActual + ")");
             System.out.println("Valor actual: " + hojaCalculo[filaActual][columnaActual]);
             System.out.print("Ingrese un comando (w/a/s/d) o presione Enter para entrar o F para salir del sistema: ");
             String comando = scanner.nextLine();
+              if (!comando.isEmpty()) {
+                hojaCalculo[filaActual][columnaActual] = completarConEspacios(comando);
+            }
             
-        }while(!salirDelSistema);
         
 
 
@@ -57,6 +62,22 @@ public class HojaCalculo{
                
             }
 
+        if(filaActual>=0 && filaActual < filas && columnaActual >= 0 && columnaActual < columnas){
+             System.out.println("Posición válida.");
+        }else {
+              System.out.println("Posición inválida. Los límites son 0-" + (filas - 1) + " para filas y 0-" + (columnas - 1) + " para columnas.");
+        }
+
+        }while(!salirDelSistema);
+
+        private static String completarConEspacios(String input) {
+        if (input.length() > 6) {
+            return input.substring(0, 6);
+        } else if (input.length() < 6) {
+            return String.format("%6s", input);
+        }
+        return input;
+    }
 
 
 
