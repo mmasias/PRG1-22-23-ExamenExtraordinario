bd9da8d3b9d8f582b8288acfcc7b90fd0d15b602
brendalopes <brenda14lopess@gmail.com>
2023-07-03 10:03:51 +0200

    mejoras en la matriz y en los movimientos

--- lopesBrenda/VisiCalc.java
+++ lopesBrenda/VisiCalc.java
@@ -9,83 +9,76 @@ public class VisiCalc {
         boolean terminar = false;
 
         int[][] hojaCalc = {
-                {1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
                 {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,},
+
         };
-        int[] posicionUsuario = {2, 1};
-
-
-
+        int[] posicionUsuario = {0, 1};
 
         do {
-
+            System.out.println("+---------------------------------------------------------------------+");
+            System.out.println("|  A   |  B   |  C   |  D   |  E   |  F   |  G   |  H   |  I   |  J   |");
+            System.out.println("|---------------------------------------------------------------------|");
             for (int fila = 0; fila < hojaCalc.length; fila++) {
                 for (int columna = 0; columna < hojaCalc[fila].length; columna++) {
                     if (fila == posicionUsuario[0] && columna == posicionUsuario[1]) {
                         System.out.print("{    }");
-                    } else {
+                    }  else {
                         if (hojaCalc[fila][columna] == 0) {
                             System.out.print("      ");
                         } else if (hojaCalc[fila][columna] == 1) {
                             System.out.print("|");
                         } else if (hojaCalc[fila][columna] == 2) {
                             System.out.print("------");
-                        } else if (hojaCalc[fila][columna] == 3) {
-                            System.out.print("===");
                         }
                     }
                 }
                 System.out.println();
+
             }
+            System.out.println("|-----------------------------------------------------------------------|");
+            System.out.println("| X:"+posicionUsuario[0]+" |  Y:"+(posicionUsuario[1])+" |");
+            System.out.println("|-----------------------------------------------------------------------|");
+            System.out.println("CMDS: [W]Arriba,[S]Abajo,[A]Izquierda,[D]Derecha,[E]Editar,[F]Salir");
+            System.out.println("Ingrese comando:");
 
 
             inputUsuario = entrada.nextLine().charAt(0);
             switch (inputUsuario) {
-                case 's', 'S', '8':
+                case 's', 'S':
                     posicionUsuario[0] = posicionUsuario[0] + 1;
                     break;
-                case 'w', 'W', '2':
+                case 'w', 'W':
                     posicionUsuario[0] = posicionUsuario[0] - 1;
                     break;
-                case 'a', 'A', '4':
-                    posicionUsuario[1] = posicionUsuario[1] - 1;
+                case 'a', 'A':
+                    posicionUsuario[1] = posicionUsuario[1] - 2;
                     break;
-                case 'd', 'D', '6':
+                case 'd', 'D':
+                    posicionUsuario[1] = posicionUsuario[1] + 2;
+                    break;
+                case 't', 'T':
                     posicionUsuario[1] = posicionUsuario[1] + 1;
                     break;
+                case 'f', 'F':
+                    terminar = true;
             }
 
         } while (!terminar);
+
+
     }
 }
