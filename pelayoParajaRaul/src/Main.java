314708b4bf26d269bb536cd14a6f838cea499f2d
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2023-07-03 09:50:37 +0200

    Add files via upload

--- pelayoParajaRaul/src/Main.java
+++ pelayoParajaRaul/src/Main.java
@@ -5,58 +5,62 @@ public class Main {
         Scanner entrada;
         int alto=10;
         int ancho=15;
-        int[][] mapa = new int[alto][ancho];
+
         boolean terminar = false;
         int[] posicionInicial = { 1, 1 };
         entrada = new Scanner(System.in);
 
+        int[][] mapa = {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                {2, 0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+                { 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 
+        };
         int limiteIzquierdo=0;
         int limiteDerecho=mapa[0].length-1;
 
 
 
-        imprimirMapa(mapa);
-        System.out.println("Posicion actual: " + posicionInicial[0] + "," + posicionInicial[1]);
-        comandos();
+            comandos();
+            imprimirMapa2(mapa, posicionInicial);
 
 
 
-        char inputUsuario = entrada.nextLine().charAt(0);
-        switch (inputUsuario) {
-            case 's', 'S' -> posicionInicial[0] = posicionInicial[0] + 1;
-            case 'w', 'W' -> posicionInicial[0] = posicionInicial[0] - 1;
-            case 'a', 'A'  -> posicionInicial[1] = posicionInicial[1] - 1;
-            case 'd', 'D'  -> posicionInicial[1] = posicionInicial[1] + 1;
-            case 'v', 'V' -> {
-                System.out.println("Ingrese el valor de la casilla");
-                int valor = entrada.nextInt();
-                mapa[posicionInicial[0]][posicionInicial[1]] = valor;
 
+
+
+                System.out.println("Ingrese un comando");
+                char inputUsuario = entrada.nextLine().charAt(0);
+                switch (inputUsuario) {
+                    case 's', 'S' -> posicionInicial[0] = posicionInicial[0] + 1;
+                    case 'w', 'W' -> posicionInicial[0] = posicionInicial[0] - 1;
+                    case 'a', 'A'  -> posicionInicial[1] = posicionInicial[1] - 1;
+                    case 'd', 'D'  -> posicionInicial[1] = posicionInicial[1] + 1;
+                    case 'v', 'V' -> {
+                        System.out.println("Ingrese el valor de la casilla");
+                        int valor = entrada.nextInt();
+                        mapa[posicionInicial[0]][posicionInicial[1]] = valor;
+
+
+                    }
+                    case 'f', 'F' -> terminar = true;
+                }
+
+
+                if (terminar) {
+                    System.out.println("Terminando programa");
+                } else {
+                    System.out.println("Posicion actual: " + posicionInicial[0] + "," + posicionInicial[1]);
+                    System.out.println("Valor actual: " + mapa[posicionInicial[0]][posicionInicial[1]]);
+                }
             }
-            case 'f', 'F' -> terminar = true;
-    }
-        if (posicionInicial[0] < limiteIzquierdo) {
-            posicionInicial[0] = limiteIzquierdo;
-        } else if (posicionInicial[0] > limiteDerecho) {
-            posicionInicial[0] = limiteDerecho;
-        }
-
-        if (terminar) {
-            System.out.println("Terminando programa");
-        } else {
-            System.out.println("Posicion actual: " + posicionInicial[0] + "," + posicionInicial[1]);
-            System.out.println("Valor actual: " + mapa[posicionInicial[0]][posicionInicial[1]]);
-        }
-}
-    public static void imprimirMapa(int[][] mapa) {
-        for (int[] fila : mapa) {
-            for (int casilla : fila) {
-                System.out.print(casilla + " ");
-            }
-            System.out.println();
-        }
-    }
 
 
     public static void comandos() {
@@ -68,7 +72,21 @@ public class Main {
         System.out.println("F: Finalizar");
     }
 
-    
+    public static void imprimirMapa2(int[][] mapa, int[] posicionActual) {
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[0].length; j++) {
+                if (i == posicionActual[0] && j == posicionActual[1]) {
+                    System.out.print("[      ]");
+                } else {
+                    System.out.print(mapa[i][j] + "      ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+
 
 
 }
