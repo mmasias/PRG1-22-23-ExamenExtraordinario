5853db003f7761f214e58361bdb75c004b7dd603
andriuw08 <andriuw.yepez@gmail.com>
2023-07-03 08:42:12 +0200

    Estructura basica de la clase VisiCalc

---
+++ yepezAndriuw/VisiCalc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class VisiCalc {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        char inputUsuario;
+        
+    }
+}
